7dae6b3420ef39c79bd5c0db57e95043d3161106
denklp <153329751+denklp@users.noreply.github.com>
2025-06-02 17:41:29 +0100

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,24 +1,30 @@
 #!/bin/bash
 
-TEMPLATE_FILE=${1:-template.yaml}
-NODEPOOL_FILE=${2:-nodepool_name.yaml}
-OUTPUT_FILE=${3:-out/merged.yaml}
-
-mkdir -p "$(dirname "$OUTPUT_FILE")"
+DEFAULT=nodepool_default.yaml
+WORKLOAD=nodepool_workload.yaml
+OUTPUT=merged_nodepool.yaml
 
 yq eval-all '
-  # Create a custom merge for "requirements"
-  def merge_requirements(a; b):
-    reduce (a + b)[] as $item (
-      {}; .[$item.key] = $item
-    ) | to_entries | map(.value);
+  # Load files
+  select(fileIndex == 0) as $default |
+  select(fileIndex == 1) as $workload |
 
-  # Main logic
-  (
-    .[0] as $base |
-    .[1] as $override |
+  # Start with merged base
+  ($default * $workload) as $merged
+  |
 
-    $base * $override |
-    .requirements = merge_requirements($base.requirements; $override.requirements)
-  )
-' "$TEMPLATE_FILE" "$NODEPOOL_FILE" > "$OUTPUT_FILE"
+  # Merge requirements with workload taking priority by key
+  $merged
+  | .temlplate.spec.requirements = (
+      ($workload.temlplate.spec.requirements // [])
+      + ($default.temlplate.spec.requirements // [])
+      | unique_by(.key)
+    )
+
+  # Merge taints the same way
+  | .temlplate.spec.taints = (
+      ($workload.temlplate.spec.taints // [])
+      + ($default.temlplate.spec.taints // [])
+      | unique_by(.key)
+    )
+' "$DEFAULT" "$WORKLOAD" > "$OUTPUT"
