7404420e869c2a5a4e364da2795e89c348fa2d1f
denklp <153329751+denklp@users.noreply.github.com>
2025-06-02 10:28:35 +0100

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,25 +1,24 @@
-yq eval-all '
-  (
-    reduce (.[0] + .[1])[] as $item (
-      {};  # start with empty map
-      .[$item.key] = $item
-    )
-  | to_entries | map(.value)
-  )
-' template.yaml nodepool_name.yaml
-
-
-#!/bin/sh
+#!/bin/bash
 
 TEMPLATE_FILE=${1:-template.yaml}
 NODEPOOL_FILE=${2:-nodepool_name.yaml}
+OUTPUT_FILE=${3:-out/merged.yaml}
+
+mkdir -p "$(dirname "$OUTPUT_FILE")"
 
 yq eval-all '
+  # Create a custom merge for "requirements"
+  def merge_requirements(a; b):
+    reduce (a + b)[] as $item (
+      {}; .[$item.key] = $item
+    ) | to_entries | map(.value);
+
+  # Main logic
   (
-    reduce (.[0] + .[1])[] as $item (
-      {};  # start with empty map
-      .[$item.key] = $item
-    )
-  | to_entries | map(.value)
+    .[0] as $base |
+    .[1] as $override |
+
+    $base * $override |
+    .requirements = merge_requirements($base.requirements; $override.requirements)
   )
-' "$TEMPLATE_FILE" "$NODEPOOL_FILE"
+' "$TEMPLATE_FILE" "$NODEPOOL_FILE" > "$OUTPUT_FILE"
